09838ac22fca1c54dfa57865745b847160704761
anmol1222 <125978506+anmol1222@users.noreply.github.com>
2026-05-14 14:17:09 +0530

    Add files via upload

---
+++ dep_app.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import pickle
+import streamlit as st
+
+model=pickle.load(open("house_model.pkl",'rb'))
+
+st.title("The Prediction of price of the houses")
+size=st.number_input("Enter the size")
+bedrooms=st.slider("select range",1,6)
+
+if st.button("Predict"):
+    features=np.array([[size,bedrooms]])
+    prediction=model.predict(features)[0]
+st.success(f"Predicted Price: ₹{prediction:,.2f}")
