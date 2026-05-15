ca81a9867be297b95bbf95cec069013729f89688
anmol1222 <125978506+anmol1222@users.noreply.github.com>
2026-05-15 15:21:55 +0530

    Enhance prediction button with snow animation
    Added a snow animation effect to the prediction button.

--- dep_app.py
+++ dep_app.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
+from streamlit_extras.let_it_rain import rain
 
 model=pickle.load(open("house_model.pkl",'rb'))
 
@@ -15,4 +16,4 @@ if st.button("Predict"):
         features=np.array([[size,bedrooms]])
         prediction=model.predict(features)[0]
     st.success(f"Predicted Price: ₹{prediction:,.2f}")
-    st.balloons()
+    st.snow(emoji='❄️',font_size=10, falling_speed=2.3, animation_length='infinite')
