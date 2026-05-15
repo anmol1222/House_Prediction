0dcced08d8883bbca0a50c0fecf8baafc7747df6
anmol1222 <125978506+anmol1222@users.noreply.github.com>
2026-05-15 15:16:56 +0530

    Enhance prediction button with spinner and balloons
    Added a loading spinner and balloons for prediction feedback.

--- dep_app.py
+++ dep_app.py
@@ -10,6 +10,9 @@ size=st.number_input("Enter the size")
 bedrooms=st.slider("select range",1,6)
 
 if st.button("Predict"):
-    features=np.array([[size,bedrooms]])
-    prediction=model.predict(features)[0]
-st.success(f"Predicted Price: ₹{prediction:,.2f}")
+
+    with st.spinner("Predicting..."):
+      features=np.array([[size,bedrooms]])
+      prediction=model.predict(features)[0]
+    st.success(f"Predicted Price: ₹{prediction:,.2f}")
+    st.balloons()
