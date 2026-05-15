150d583685fb3bce3ff80b07b5faf1b7cbda5d94
anmol1222 <125978506+anmol1222@users.noreply.github.com>
2026-05-15 15:18:05 +0530

    Update dep_app.py

--- dep_app.py
+++ dep_app.py
@@ -10,9 +10,9 @@ size=st.number_input("Enter the size")
 bedrooms=st.slider("select range",1,6)
 
 if st.button("Predict"):
-
+    
     with st.spinner("Predicting..."):
-      features=np.array([[size,bedrooms]])
-      prediction=model.predict(features)[0]
+        features=np.array([[size,bedrooms]])
+        prediction=model.predict(features)[0]
     st.success(f"Predicted Price: ₹{prediction:,.2f}")
     st.balloons()
